f7de9b382079bf5c29b4df9d6b2444f672625be1
hj1115hj <hyejin_one@naver.com>
2023-02-02 00:40:20 +0900

    remember, by

--- app/src/main/java/com/example/myapplication/WaterCounter.kt
+++ app/src/main/java/com/example/myapplication/WaterCounter.kt
@@ -5,9 +5,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -17,12 +15,11 @@ import androidx.compose.ui.unit.dp
 @Composable
 fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        val count: MutableState<Int> = mutableStateOf(0)
+        var count by remember { mutableStateOf(0) }
         Text("You've had $count glasses.")
-        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
+        Button(onClick = { count++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
-
     }
 }
 
