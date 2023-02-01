9273baca8b997753c03494c04b4dc22c06da95e2
hj1115hj <hyejin_one@naver.com>
2023-02-02 00:01:09 +0900

    initailize

---
+++ app/src/main/java/com/example/myapplication/WellnessScreen.kt
@@ -0,0 +1,10 @@
+package com.example.myapplication
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun WellnessScreen(modifier: Modifier = Modifier){
+    WaterCounter(modifier)
+}
