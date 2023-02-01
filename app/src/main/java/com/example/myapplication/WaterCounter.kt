4a4d19fc73d57ad023f2c353cb4abcc068761111
hj1115hj <hyejin_one@naver.com>
2023-02-02 00:26:11 +0900

    mutable state

--- app/src/main/java/com/example/myapplication/WaterCounter.kt
+++ app/src/main/java/com/example/myapplication/WaterCounter.kt
@@ -1,20 +1,25 @@
 package com.example.myapplication
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+
+@SuppressLint("UnrememberedMutableState")
 @Composable
-fun WaterCounter(modifier: Modifier = Modifier){
+fun WaterCounter(modifier: Modifier = Modifier) {
     Column(modifier = modifier.padding(16.dp)) {
-        var count = 0
+        val count: MutableState<Int> = mutableStateOf(0)
         Text("You've had $count glasses.")
-        Button(onClick = {count++}, Modifier.padding(top= 8.dp)){
+        Button(onClick = { count.value++ }, Modifier.padding(top = 8.dp)) {
             Text("Add one")
         }
 
@@ -23,6 +28,6 @@ fun WaterCounter(modifier: Modifier = Modifier){
 
 @Preview(showBackground = true)
 @Composable
-fun WellnessScreenPreview(){
+fun WellnessScreenPreview() {
     WellnessScreen()
 }
