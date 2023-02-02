8d60188a83892e232edf4d6927a0f21729be5744
hj1115hj <hyejin_one@naver.com>
2023-02-03 01:12:38 +0900

    Compose에서 상태 호이스팅

--- app/src/main/java/com/example/myapplication/WellnessScreen.kt
+++ app/src/main/java/com/example/myapplication/WellnessScreen.kt
@@ -1,11 +1,12 @@
 package com.example.myapplication
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
-fun WellnessScreen() {
-    WaterCounter()
+fun WellnessScreen(modifier: Modifier = Modifier) {
+    StatefulCounter(modifier)
 }
 
 @Preview(showBackground = true)
